5b522937e34b8a0ed9c3404ab0af611556118c02
HanaaHyderAli <hzn10296@yahoo.com>
2025-05-27 19:46:43 +0400

    Fix: case-sensitive component import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import Categoriesgrid from '../components/Categoriesgrid'
 import Reviews from '../components/Reviews'
